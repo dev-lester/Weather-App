70a611cb00f51ce5e88c6d84ab016d052cd4103e
lester <lesters821@gmail.com>
2020-08-12 04:51:13 +0800

    Initial commit

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ const bodyParser = require('body-parser')
 
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/' , (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
-    
+
 });
 
 app.post('/', (req, res) => {
 
     const query = req.body.cityName;
-    const apiKey = "90e48562805399ce7f9e187ca036a4ed";
+    const apiKey = "Your API key here"; // register to open-weather to get your api key https://openweathermap.org/
     const unit = "metric";
 
     const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
@@ -24,12 +26,12 @@ app.post('/', (req, res) => {
 
         response.on('data', (data) => {
             const weatherData = JSON.parse(data);
-            const discription = weatherData.weather[0].description 
+            const discription = weatherData.weather[0].description
             const temp = weatherData.main.temp;
-            const icon = weatherData.weather[0].icon 
-            const iconUrl = 'https://openweathermap.org/img/wn/'+ icon +'@2x.png';
+            const icon = weatherData.weather[0].icon
+            const iconUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
             res.write("<p>Weather discription " + discription + "</p>");
-            res.write("<h1>The temperature in " + query + " is "  + temp + " degrees celcius.</h1>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees celcius.</h1>");
             res.write("<img src=" + iconUrl + ">");
             res.send();
         });
